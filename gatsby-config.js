afb34cd3db1e7ed6cdae5d32a2fb554923210984
Youngjae <limyoungjae@gmail.com>
2020-12-03 20:47:25 -0500

    Updated siteMetadata and bio.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Gatsby Code Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Youngjae Jay Lim`,
+      summary: `who lives and works in NYC building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `@JayYoungjae`,
     },
   },
   plugins: [
